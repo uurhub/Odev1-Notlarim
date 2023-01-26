633aa09256e050847109cd20d6bbabd5251f6141
Ugur Gorkem <ugurgorkem.odabas@gmail.com>
2023-01-26 11:08:55 +0300

    Collection Method'ları

--- bin/dart_application_1.dart
+++ bin/dart_application_1.dart
@@ -1,8 +1,32 @@
 void main() {
-  final List<int> l = [1,2,3];
-  final Set<int> s = {1,2,3};
-  final Map<int,int> m = {1:10,2:20,3:30}; 
+  final iterable = [1,2,3,4,5,6,7,8,9,0];
+  print(iterable);
+  print(iterable.first);
+  print(iterable.last);
+  print(iterable.firstWhere((element) => element % 4 == 0));
 
-  print([0,...l,4,5,6]);
+
+  if (iterable.every((element) => element % 3 == 0 )){
+    print("sağladı1");
+  }else if (iterable.any((element) => element % 5 == 0)){
+    print("sağladı2");
+  }else{
+    print("sağlamadı");
+  }
+
+
+  print(iterable.where((element) => element > 4));
+  print(iterable.where((element) => element < 4));
+
+
+  print(iterable.map((e) => "$e sayisi"));
+
+
+
+
+
+
+
+
 
 }
